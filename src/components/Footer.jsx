2ed7017db52caf3901adfc8a53ad340cce5b1775
abeonweb <bmospecific@yahoo.com>
2023-12-12 09:58:54 +0100

    footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="bg-gray-800 h-[80px] flex flex-col justify-end items-center">
+      <p className="text-gray-400 text-xs pb-2">&copy; Target Translation S.L</p>
+    </section>
   )
 }
 
