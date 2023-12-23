5605ce9200d9a44568de740d94d6206d48198c65
abeonweb <bmospecific@yahoo.com>
2023-12-23 10:08:28 +0100

    added proper footer tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className="bg-gray-800 h-[80px] flex flex-col justify-end items-center">
-      <p className="text-gray-400 text-xs pb-2">&copy; Target Translations 2023</p>
-    </section>
+    <footer className="bg-gray-800 h-[100px] flex flex-col justify-end items-center">
+      <p className="text-gray-400 text-xs pb-4">&copy; Target Translations 2023</p>
+    </footer>
   )
 }
 
